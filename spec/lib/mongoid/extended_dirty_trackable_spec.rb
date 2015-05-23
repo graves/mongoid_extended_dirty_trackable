a92fff9db68cbabc8d9c8761fc861974077deba4
Thomas Graves <thomas@ooo.pm>
2015-05-23 10:27:15 -0400

    wired in test harness, pass tests for embeds_one
    create models in spec file for now, i'll extract them later.
    tests require mongodb to be running on localhost default port.
    the db setup and teardown is done with convenience methods in
    the spec helper and will probably stay there. the embeds_one
    spec reveales a bug in the Concern with the @embedded_Changes
    conditional assignment. sometimes the hash is empty so the
    expression ins't evaluated leaving us with an empty changes hash

--- spec/lib/mongoid/extended_dirty_trackable_spec.rb
+++ spec/lib/mongoid/extended_dirty_trackable_spec.rb
@@ -1,11 +1,53 @@
 require_relative '../../spec_helper'
+require_relative '../../../lib/mongoid_extended_dirty_trackable.rb'
+require 'pry'
+
+class Account
+  include Mongoid::Document
+  include Mongoid::ExtendedDirtyTrackable
+
+  field :name
+
+  embeds_one :address
+end
+
+class Address
+  include Mongoid::Document
+
+  field :zipcode
+
+  embedded_in :account
+end
 
 describe Mongoid::ExtendedDirtyTrackable do
   it 'has a version number' do
     expect(Mongoid::ExtendedDirtyTrackable::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'it is wired in correctly as a mixin' do
+    expect(Account.new.singleton_class < Mongoid::ExtendedDirtyTrackable)
+      .to eq(true)
   end
 end
+
+describe Account do
+  it "detects changes to itself" do
+    account = Account.new(:name => "Umbrella Corp")
+    account.save!
+    account.name = "Prestige Worldwide"
+
+    expect(account.changed?).to eq(true)
+  end
+
+  it "detects changes to embedded_one documents" do
+    account = Account.new
+    account.create_address(zipcode: "90210")
+    account.address.zipcode = "1000 AS"
+
+    expect(account.changed?).to eq(true)
+    expect(account.changes["zipcode"]).to eq(["90210", "1000 AS"])
+  end
+end
+
+
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,15 @@ require 'mongoid'
 require 'mongoid_extended_dirty_trackable'
 
 require 'rspec'
+
+Mongoid.configure do |config|
+  name = "mongoid_test_db"
+  host = "localhost"
+  config.connect_to(name)
+end
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    Mongoid.purge!
+  end
+end
